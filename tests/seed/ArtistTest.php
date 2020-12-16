391f1c61a4823bbde783da7ce841ad4199168c47
datagutten <datagutten@datagutten.net>
2020-12-16 13:39:32 +0100

    Add tests for Artist class

---
+++ tests/seed/ArtistTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace seed;
+
+use datagutten\musicbrainz\seed\Artist;
+use PHPUnit\Framework\TestCase;
+
+class ArtistTest extends TestCase
+{
+
+    public function testSave()
+    {
+        $artist = new Artist(['mbid' => '710e234d-e1e2-4f30-89ec-01bd71f6e4a3', 'name' => 'OsloEss', 'artist_name' => 'Oslo Ess', 'join_phrase' => 'feat.']);
+        $fields = $artist->save('artist.0.');
+        $this->assertSame([
+            'artist.0.mbid' => '710e234d-e1e2-4f30-89ec-01bd71f6e4a3',
+            'artist.0.name' => 'OsloEss',
+            'artist.0.artist_name' => 'Oslo Ess',
+            'artist.0.join_phrase' => 'feat.'
+        ], $fields);
+    }
+
+    public function testArtist()
+    {
+        $artist = new Artist(['mbid' => '710e234d-e1e2-4f30-89ec-01bd71f6e4a3', 'name' => 'OsloEss', 'artist_name' => 'Oslo Ess', 'join_phrase' => 'feat.']);
+        $this->assertSame('710e234d-e1e2-4f30-89ec-01bd71f6e4a3', $artist->mbid);
+        $this->assertSame('OsloEss', $artist->name);
+        $this->assertSame('Oslo Ess', $artist->artist_name);
+        $this->assertSame('feat.', $artist->join_phrase);
+    }
+}

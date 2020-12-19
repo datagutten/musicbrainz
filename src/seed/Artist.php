1ccf9557f93f440d3c7f5c2c8f1f0f7b7b987842
datagutten <datagutten@datagutten.net>
2020-12-19 09:28:15 +0100

    Add field descriptions to artist

--- src/seed/Artist.php
+++ src/seed/Artist.php
@@ -7,9 +7,22 @@ namespace datagutten\musicbrainz\seed;
 class Artist extends Element
 {
     protected $fields = ['mbid', 'name', 'artist_name', 'join_phrase'];
+    /**
+     * @var string Artist MBID
+     */
     public $mbid;
+    /**
+     * @var string The name of the artist, as credited on the release. Optional, if omitted it will default to the artist’s current name
+     *
+     */
     public $name;
+    /**
+     * @var string The name of the artist as it is usually referred too. Optional if you already specified both credited name and MBID
+     */
     public $artist_name;
+    /**
+     * @var string An optional phrase to join this artist with the next artist. For example, you could use “ & ” to join “Calvin” with “Hobbes” to get the final text “Calvin & Hobbes”
+     */
     public $join_phrase;
 
     public function __construct(array $args)

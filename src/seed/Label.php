99262092565cfd4053f8a1e4bf9f9f57052db3b3
datagutten <datagutten@datagutten.net>
2022-10-09 21:24:41 +0200

    Add releases to label

--- src/seed/Label.php
+++ src/seed/Label.php
@@ -9,10 +9,25 @@ class Label extends Element
     public static string $entity = 'label';
     public string $mbid;
     public string $name;
+    public string $catalog_number;
     protected array $fields = ['id', 'catalog_number', 'name'];
+
+    /**
+     * @var Release[] Label releases
+     */
+    public array $releases;
 
     public function __construct(array $args)
     {
         $this->register_fields($args);
+        if (!empty($args['releases']))
+        {
+            foreach ($args['releases'] as $release)
+            {
+                $release_obj = new Release($release);
+                $release_obj->labels[] = $this;
+                $this->releases[] = $release_obj;
+            }
+        }
     }
 }
